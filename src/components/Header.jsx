aa7607ebcd8b212235ca54b3bf74b9b902431d24
Castelli87 <davidecastelli87@hotmail.com>
2023-07-25 16:36:08 +0100

    Start create the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
-const Header =()=>{
-    return (
-        <header>
-           <h1 className="text-3xl font-bold underline">Header</h1> 
-
-        </header>
-    )
-}
+const Header = () => {
+  return (
+    <header className="flex justify-between bg-primary ">
+      <a href="/" className="">
+        Logo
+      </a>
+      <nav >
+        <ul className="flex justify-between">
+          <li>
+            <a href="/#AboutMe" className="p-2 m-2 text-gray-600	hover:text-black hover:underline ">About</a>
+          </li>
+          <li>
+            <a href="/#Projects" className="p-2 m-2 text-gray-600	hover:text-black hover:underline">Projects</a>
+          </li>
+          <li>
+            <a href="/#Contact" className="p-2 m-2 text-gray-600	hover:text-black hover:underline">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
