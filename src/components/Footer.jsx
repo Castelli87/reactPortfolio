e0cce25a2fc35998c28ce1c7afb68f685f3672fb
Castelli87 <davidecastelli87@hotmail.com>
2023-08-01 10:35:51 +0100

    Done the backToTop function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,27 @@
-import { FiLinkedin, FiGithub } from "react-icons/fi";
+import { FiLinkedin, FiGithub, FiArrowUp } from "react-icons/fi";
 import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+
   return (
     <section>
+      <div className="flex justify-end">
+        <button onClick={scrollToTop}className="flex md:m-3 md:text-left bg-blue-500  hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ">
+          <FiArrowUp className=" text-3xl" />
+        </button>
+      </div>
       <h2 className="text-3xl ">Follow Me</h2>
+
       <div className="flex justify-center mt-7">
-        <a href="https://github.com/Castelli87" >
+        <a href="https://github.com/Castelli87">
           <FiGithub className="ml-5 text-2xl text-gray-500 hover:text-indigo-700" />
         </a>
         <a href="https://www.linkedin.com/in/davidecastelli87/">
@@ -15,7 +30,9 @@ const Footer = () => {
       </div>
       <div className="flex justify-center mt-8  ">
         <FaRegCopyright className="text-md text-gray-600  " />
-        <span className="text-sm ml-1">2023 Davide Castelli. All right reserved.</span>
+        <span className="text-sm ml-1">
+          2023 Davide Castelli. All right reserved.
+        </span>
       </div>
     </section>
   );
