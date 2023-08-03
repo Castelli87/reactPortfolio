e9485d723892a97f04abbdacf4f7488fa89bcb99
Castelli87 <davidecastelli87@hotmail.com>
2023-08-03 09:21:36 +0100

    Fixed problem with white text in dark mode

--- src/components/Concact.jsx
+++ src/components/Concact.jsx
@@ -61,7 +61,7 @@ const handleSubmit=(event)=>{
           <input
             type="email"
             id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-200 focus:border-blue-200 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-200 focus:border-blue-200 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
             name="email"
             value={inputValues.email}
             onChange={handleChange}
@@ -97,7 +97,7 @@ const handleSubmit=(event)=>{
           <textarea
             id="message"
             rows="4"
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Please leave your message here.... "
             onChange={handleChange}
             name="message"
