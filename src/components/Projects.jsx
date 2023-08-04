2b6d345b624b3577facf8663055c44507d2c2f25
Castelli87 <davidecastelli87@hotmail.com>
2023-08-04 23:04:16 +0100

    modal pop-up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import projects from "../assets/data/data";
+import SingleProjects from "./SingleProject";
 
 
 const Projects = () => {
+
+  const [showModal,setShowModal]= useState(false )
+  const [projId,setProjId]=useState('')
+
+
   return (
     <section className=" container  grid  mt-10 md:grid-cols-2 lg:grid-cols-3 gap-10"id='Projects'>
     
@@ -13,13 +20,16 @@ const Projects = () => {
             <p className="text-gray-700 text-left mt-3 dark:bg-slate-800 dark:text-white" > - {item.category}</p>
             <p className="text-gray-700 text-left dark:bg-slate-800 dark:text-white"> - {item.type}</p>
             <div className="flex justify-end	dark:bg-slate-800 dark:text-white">
-            <button className=" bg-blue-500 mt-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" ><a>More Info</a></button>
-
+            <button className=" bg-blue-500 mt-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() =>{setShowModal(true)
+            setProjId(item)
+            } }>More Info</button>
             </div>
           </div>
         </div>
       ))}
-  
+      {showModal ? <SingleProjects proj={projId} setShowModal={setShowModal}/>: null}
+
+      
 
     
     </section>
---
+++ src/components/SingleProject.jsx
@@ -0,0 +1,56 @@
+const SingleProjects =({proj,setShowModal})=>{
+
+return (
+<div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 bg-gray-100 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                  <h3 className="text-3xl font=semibold">General Info</h3>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+                      x
+                    </span>
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                 <h1>{proj.id}</h1>
+                 <h2>{proj.TechStack}</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+)
+
+}  
+
+export default SingleProjects
+
+/* 
+ { project= projects.find((element) => element.id === projId);}
+        <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                  <h3 className="text-3xl font=semibold">General Info</h3>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+                      x
+                    </span>
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                 <h1>{projId}</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+*/
